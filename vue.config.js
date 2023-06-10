a1aea3366fef9443151aeba64963630e68b8a4fd
nnaren <nnaren@163.com>
2023-06-10 22:37:06 +0800

    h哈哈

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,22 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+// webpack.config.js
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+
+
+module.exports = {
+  configureWebpack: config => {
+    config.plugins.push(
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      })
+    )
+  }
+
+}
